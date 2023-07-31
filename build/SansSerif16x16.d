746261ab27e135a388677451849888df15830e33
Oğuzhan Büyüksolak <80602263+oguzhanbsolak@users.noreply.github.com>
2023-07-31 19:38:17 +0300

    Ignore build and db

--- build/SansSerif16x16.d
+++
@@ -1,2 +0,0 @@
-C:/Users/obuyukso/Desktop/desktop_code/facial_recognition_ai87_mobilefacenet_112_cos_emb_gen/build/SansSerif16x16.o: \
- TFT/fthr/SansSerif16x16.c
--- build/startup_max78002.d
+++
@@ -1,2 +0,0 @@
-C:/Users/obuyukso/Desktop/desktop_code/facial_recognition_ai87_mobilefacenet_112_cos_emb_gen/build/startup_max78002.o: \
- C:/MaximSDK/Libraries/CMSIS/Device/Maxim/MAX78002/Source/GCC/startup_max78002.S
