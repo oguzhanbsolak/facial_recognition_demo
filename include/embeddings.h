86d5664ab77d6ad7f89f21ac2724878b663a3fc2
Buyuksolak <Oguzhan.Buyuksolak@analog.com>
2023-08-18 15:23:33 +0300

    Removed unnecessary serial dependency

--- include/embeddings.h
+++ include/embeddings.h
@@ -1,8 +1,7 @@
-#define DEFAULT_EMBS_NUM 48 
+#define DEFAULT_EMBS_NUM 36 
 
 #define DEFAULT_NAMES { \
  "Ashton", "Ashton", "Ashton", "Ashton", "Ashton", "Ashton", "BradPi", "BradPi", "BradPi", "BradPi", "BradPi", "BradPi", "Charli", "Charli", "Charli", \
- "Charli", "Charli", "Charli", "ChrisH", "ChrisH", "ChrisH", "ChrisH", "ChrisH", "ChrisH", "Erman", "Erman", "Erman", "Erman", "Erman", "Erman", \
- "MilaKu", "MilaKu", "MilaKu", "MilaKu", "MilaKu", "MilaKu", "Oguzha", "Oguzha", "Oguzha", "Oguzha", "Oguzha", "Oguzha", "Scarle", "Scarle", "Scarle", \
- "Scarle", "Scarle", "Scarle" \
+ "Charli", "Charli", "Charli", "ChrisH", "ChrisH", "ChrisH", "ChrisH", "ChrisH", "ChrisH", "MilaKu", "MilaKu", "MilaKu", "MilaKu", "MilaKu", "MilaKu", \
+ "Scarle", "Scarle", "Scarle", "Scarle", "Scarle", "Scarle" \
 }
